fe2dbf8c19189fedda6fc7229950c304c9844128
jessica <samandjesslovepie@gmail.com>
2017-07-06 16:50:35 -0600

    mini project

--- src/App.js
+++ src/App.js
@@ -1,19 +1,64 @@
 import React, { Component } from 'react';
 import './styles/App.css';
+import {getTroops} from './services/getTroops';
+import {postTroop} from './services/postTroop';
+import {getEnemies} from './services/enemies';
 
 
 class App extends Component {
+  constructor(props) {
+    super(props);
 
+    this.state = {
+      armiesArray: [],
+      defensesArray: [],
+      newRecruit: ''
 
-
+    }
+    this.seeEnemies = this.seeEnemies.bind(this)
+    // this.recruitTroop=this.recruitTroop.bind(this)
+    // this.handleInput = this.handleInput.bind(this)
+    this.callTroops = this.callTroops.bind(this)
+  }
 
   seeEnemies() {
+    // console.log('clicked');
+    getEnemies().then(apiData => {
+      this.setState({
+        armiesArray: apiData
+      })
+    })
   }
 
   callTroops() {
+    getTroops().then(response => {
+      this.setState({
+        defensesArray: response
+      })
+    })
   }
 
-  recruitTroop() {
+  componentDidMount() {
+    this.callTroops();
+  }
+
+//   recruitTroop(event, recruit) {
+//     event.preventDefault();
+//
+//     if (recruit) {
+//       postTroop(recruit).then(() => {
+//         this.callTroops();
+//         this.setState({
+//           newRecruit:''
+//         })
+//       })
+//   }
+// }
+
+  handleInput(event) {
+    this.setState({
+      newRecruit: event.target.value
+    })
   }
 
 
@@ -25,7 +70,22 @@ class App extends Component {
 
 
   render() {
-
+    const armies = this.state.armiesArray.map((army, armyIndex) => (
+      <ul className='army' key={armyIndex}>
+        <h3>Enemy Army #{army.id}: {army.name}</h3>
+        <div className='leader'>{army.leader}</div>
+        <ul className='minions'>
+          {army.minions.map((minion, minionIndex) => (
+            <li key={minionIndex} className="minion">{minion.type}</li>
+            ))}
+          </ul>
+      </ul>
+    ))
+    //
+    const troops = this.state.defensesArray.forEach((troop, troopIndex) => {
+      // return <li className='troop' key={ troopIndex }>{troop.recruit}</li>
+      console.log( troop )
+    })
 
     const message = this.state && this.state.armiesArray.length < 1 ? "ALL CLEAR" : "";
 
@@ -37,7 +97,7 @@ class App extends Component {
           <h1>Enemies at our gate!</h1>
           <h2>Prepare our defenses!</h2>
           <div className="defenses">
-            <div className="defense" id="sentry">Sentry<span className="instructions">Click here to see approaching enemies!</span></div>
+            <div className="defense" id="sentry" onClick={this.seeEnemies}>Sentry<span className="instructions">Click here to see approaching enemies!</span></div>
             <div className="defense" id="captain">Captain<span className="instructions">Fill out Request Form below to recruit new troop!</span></div>
             <div className="defense" id="wizard">Wizard<span className="instructions">Click directly on a minion to cast a spell!</span></div>
             <div className="defense" id="ballista">Ballista<span className="instructions">Blast enemy leader to disperse army!</span></div>
@@ -50,7 +110,7 @@ class App extends Component {
           <form type="submit">
             New Recruit Request Form:
             <input id="paperwork" placeholder="Please indicate requested recruit"/>
-            <button>Enlist!</button>
+            <button >Enlist!</button>
           </form>
 
           <div id="wall">
@@ -59,14 +119,14 @@ class App extends Component {
         </div>
 
         <ul className="troops">
-          
+          {troops}
         </ul>
 
         <h1 id="message">{message}</h1>
 
         {/* Enemy Armies */}
         <div className="enemies">
-
+        {armies}
         </div>
       </div>
     );
---
+++ src/services/enemies.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+import {turnApiObjIntoArray} from '../utils/turnApiObjIntoArray'
+
+
+export function getEnemies() {
+  return axios.get('http://localhost:3005/all')
+  .then( response => {
+    return turnApiObjIntoArray(response.data)
+  })
+}
+
+
+
+// export function getEnemies() {
+//   return axios.get('http://localhost:3005/all')
+//   .then(res => {
+//     return turnApiObjIntoArray(res.data)
+//   })
+// }
